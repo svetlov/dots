853b1542528a632258163409e076e24b3d18b3b4
Vsevolod Svetlov <svetlov.vsevolod@gmail.com>
2015-08-14 14:35:11 +0300

    Add initial zsh support

--- install.py
+++ install.py
@@ -28,13 +28,38 @@ class Installer(object):
 
 class DebianInstaller(Installer):
     """
-    git cmake python-dev
+    git cmake python-dev gcc g++ zsh
     """
+
+
+class OhMyZSHInstaller(Installer):
+    def install(self):
+        with open(os.path.join(HOME, ".bashrc"), 'w') as bashrc:
+            bashrc.write('[ -z "$PS1" ] && return\n\nexec zsh')
+        sb.call([
+            "git", "clone",
+            "git://github.com/robbyrussell/oh-my-zsh.git",
+            os.path.join(HOME, ".oh-my-zsh")
+        ])
+        os.symlink(
+            os.path.join(PWD, "all", "zsh", "zshrc"),
+            os.path.join(HOME, ".zshrc")
+        )
+        themes = os.path.join(HOME, ".oh-my-zsh", "custom", "themes")
+        os.mkdir(themes)
+        os.symlink(
+            os.path.join(PWD, "all", "zsh", "mytheme.zsh-theme"),
+            os.path.join(themes, "mytheme.zsh-theme")
+        )
+
 
 
 class VimInstaller(Installer):
     def install(self):
-        os.symlink(os.path.join(PWD, "all", "vimrc"), os.path.join(HOME, ".vimrc"))
+        os.symlink(
+            os.path.join(PWD, "all", "vim", "vimrc"),
+            os.path.join(HOME, ".vimrc")
+        )
         sb.call([
             "git", "clone",
             "https://github.com/VundleVim/Vundle.vim.git",
@@ -46,12 +71,11 @@ class VimInstaller(Installer):
         with PathGuard(os.path.join(HOME, ".vim", "bundle", "YouCompleteMe")):
             sb.call(["git", "submodule", "update", "--init", "--recursive"])
             sb.call(["./install.sh", "--clang-completer"])
-
-
-
+        sb.call(["zsh"])
 
 def main():
-    VimInstaller().install()
+    # VimInstaller().install()
+    OhMyZSHInstaller().install()
 
 
 if __name__ == '__main__':
