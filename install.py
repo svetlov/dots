08fe154243bea2fad7ee19f4b87e90aa5162c1fe
Vsevolod Svetlov <svetlov.vsevolod@gmail.com>
2015-08-17 17:44:47 +0300

    Add fast installer

--- install.py
+++ install.py
@@ -9,6 +9,7 @@ from contextlib import contextmanager
 
 PWD = os.getcwd()
 HOME = os.path.expanduser("~")
+INSTALLERS = {}
 
 
 @contextmanager
@@ -28,12 +29,25 @@ def symlink(source, destination, force=True):
     os.symlink(source, destination)
 
 
-class Installer(object):
-    def install(self):
-        pass
+class RegisterInstallerMetaclass(type):
+    def __init__(cls, name, bases, dct):
+        super(RegisterInstallerMetaclass, cls).__init__(name, bases, dct)
+        if cls.name is not None:
+            INSTALLERS[cls.name] = cls
 
-    def unistall(self):
-        pass
+
+class Installer(object, metaclass=RegisterInstallerMetaclass):
+    # __metaclass__ = RegisterInstallerMetaclass
+    name = None
+    description = None
+
+    @staticmethod
+    def install():
+        raise NotImplementedError
+
+    @staticmethod
+    def uninstall():
+        raise NotImplementedError
 
 
 class DebianInstaller(Installer):
@@ -42,9 +56,14 @@ class DebianInstaller(Installer):
     python-pip python3-pip
     """
 
+    name = 'debian'
+
 
 class GitConfigInstaller(Installer):
-    def install(self):
+    name = 'git'
+
+    @staticmethod
+    def install():
         symlink(
             os.path.join(PWD, "all", "git", "gitconfig"),
             os.path.join(HOME, ".gitconfig")
@@ -52,7 +71,10 @@ class GitConfigInstaller(Installer):
 
 
 class OhMyZSHInstaller(Installer):
-    def install(self):
+    name = 'zsh'
+
+    @staticmethod
+    def install():
         with open(os.path.join(HOME, ".bashrc"), 'w') as bashrc:
             bashrc.write('[ -z "$PS1" ] && return\n\nexec zsh')
         sb.call([
@@ -73,7 +95,10 @@ class OhMyZSHInstaller(Installer):
 
 
 class VimInstaller(Installer):
-    def install(self):
+    name = 'vim'
+
+    @staticmethod
+    def install():
         symlink(
             os.path.join(PWD, "all", "vim", "vimrc"),
             os.path.join(HOME, ".vimrc")
@@ -92,7 +117,10 @@ class VimInstaller(Installer):
 
 
 class TmuxInstaller(Installer):
-    def install(self):
+    name = 'tmux'
+
+    @staticmethod
+    def install():
         with PathGuard(os.path.join(PWD, "all", "tmux")):
             sb.call(["./install.sh"])
         symlink(
@@ -109,11 +137,44 @@ class TmuxInstaller(Installer):
             )
 
 
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(help='actions', dest='action')
+    install = subparsers.add_parser('install')
+    remove = subparsers.add_parser('remove')
+    show = subparsers.add_parser('list')
+
+    for p in [install, remove]:
+        p.add_argument(
+            'names', type=str, metavar='package',
+            nargs='+', choices=INSTALLERS.keys()
+        )
+    return parser.parse_args()
+
+
+def install(args):
+    for name in args.names:
+        INSTALLERS[name].install()
+
+
+def remove(args):
+    for name in args.names:
+        INSTALLERS[name].uninstall()
+
+
+def show(args):
+    for name, installer in INSTALLERS.items():
+        print("\t{}\t{}".format(name, installer.description))
+
+
 def main():
-    # VimInstaller().install()
-    OhMyZSHInstaller().install()
-    # GitConfigInstaller().install()
-    # TmuxInstaller().install()
+    args = parseArgs()
+    action = {
+        'install': install,
+        'remove': remove,
+        'list': show
+    }
+    action[args.action](args)
 
 
 if __name__ == '__main__':
